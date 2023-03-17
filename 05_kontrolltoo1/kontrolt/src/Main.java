564515cbd56d6cfc748d8c6f4400b4c20f36cf88
reigoku <reigok@tlu.ee>
2023-03-17 14:51:27 +0200

    keskmine kiirus 14:45
    aaaaaaaaa

--- 05_kontrolltoo1/kontrolt/src/Main.java
+++ 05_kontrolltoo1/kontrolt/src/Main.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        /*Scanner scanner = new Scanner(System.in);
         double input1 = Double.parseDouble(scanner.nextLine());
-        double input2 = Double.parseDouble(scanner.nextLine());
+        double input2 = Double.parseDouble(scanner.nextLine());*/
+        insertAverages insAvg = new insertAverages();
 
-
-
-
-        System.out.println("Kahe lõigu keskmine kiirus: " + ((input1 + input2) / 2);
+        System.out.println("Kahe lõigu keskmine kiirus: " + insAvg.getAverage());
+        System.out.println(insAvg.listTotalAverage());
+        System.out.println("Lõikudevaheliste keskmiste keskmine: " + insAvg.getTotalAverage());
 
 
 
--- 05_kontrolltoo1/kontrolt/src/insertAverages.java
+++ 05_kontrolltoo1/kontrolt/src/insertAverages.java
@@ -1,28 +1,66 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class insertAverages {
     double input1;
     double input2;
+    List<Double> averageSpeeds;
 
-    public insertAverages(double input1, double input2){
+
+    public insertAverages(){
         this.input1 = input1;
         this.input2 = input2;
+        this.averageSpeeds = new ArrayList<>();
     }
-    public static double getTotalAverage(double input1, double input2) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("keskkiirus.txt"));
+    public double getAverage(/*double input1, double input2*/) throws IOException {
+        BufferedReader br1 = new BufferedReader(new FileReader("kiirusSisestus.txt"));
+        input1 = Double.parseDouble(br1.readLine());
+        input2 = Double.parseDouble(br1.readLine());
+        return (input1 + input2) / 2;
+    }
+    public double listTotalAverage(){
+        averageSpeeds.add(input1);
+        averageSpeeds.add(input2);
+        double totalSpeed = 0;
+
+        for(int i = 0; i < averageSpeeds.size(); i ++){
+            totalSpeed += averageSpeeds.get(i);
+
+        }
+        return totalSpeed / averageSpeeds.size();
+
+
+    }
+    public double getTotalAverage(/*double input1, double input2*/) throws IOException {
+        /*BufferedReader br = new BufferedReader(new FileReader("kiirusVäljastus.txt"));
         double averageSpeed = Double.parseDouble(br.readLine());
         double averageTotal = 0;
         int i = 0;
-        while(averageSpeed != null){
+        while(String.valueOf(averageSpeed) != null){
             averageTotal += averageSpeed;
             i += 1;
             averageSpeed = Double.parseDouble(br.readLine());
 
-        }
+        }*/
+        BufferedReader br1 = new BufferedReader(new FileReader("kiirusSisestus.txt"));
+        input1 = Double.parseDouble(br1.readLine());
+        input2 = Double.parseDouble(br1.readLine());
+        br1.close();
+
+        BufferedReader br2 = new BufferedReader(new FileReader("kiirusVäljastus.txt"));
+        double averageTotal = Double.parseDouble(br2.readLine());
+        double i = Integer.parseInt(br2.readLine());
+        br2.close();
+
         averageTotal += input1 + input2;
+        i += 2;
         double totalAverage = averageTotal / i;
-        PrintWriter pw = new PrintWriter(new FileWriter("keskkiirus.txt"));
-        pw.print((input1 + input2)/2);
+        PrintWriter pw = new PrintWriter(new FileWriter("kiirusVäljastus.txt"));
+        pw.println((averageTotal)/i);
+        pw.println(i);
+        pw.close();
         return totalAverage;
 
     }
