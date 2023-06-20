d93a382a3f4ccb0a7426bdd356a38464ffbbbadf
reigoku <reigok@tlu.ee>
2023-06-20 10:21:30 +0300

    this code feels so spiteful
    none of this code makes sense to use the way i did, but it technically meets the requirements of 2 subclasses with 3 functions and at least 3 variables in each (sub)class

--- 03_klassikomplekt/klassikomplekt/src/Main.java
+++ 03_klassikomplekt/klassikomplekt/src/Main.java
@@ -4,7 +4,28 @@ public class Main {
 
         double data1 = 66.22;
         double data2 = 20.38;
-        NotVeryImportantInformation notVeryImportantInformation = new NotVeryImportantInformation(data1, data2);
+        double data3 = 13.40;
+        double data4 = 79.92;
+        int modifier = 5;
+        System.out.println(imReallyJustWritingThingsNowHuh(data1, data2, data3, data4, modifier));
+        System.out.println(iAddedTheseLines());
+        System.out.println(mightAsWellUseAllFunctions(data1, data2, data3, data4, modifier));
 
+    }
+    public static double imReallyJustWritingThingsNowHuh(double data1, double data2, double data3, double data4, int modifier){
+        NotVeryImportantInformation notVeryImportantInformation = new NotVeryImportantInformation(data1, data2);
+        VeryImportantInformation veryImportantInformation = new VeryImportantInformation(data3, data4, modifier);
+        double losingTrackOfTheCalculations = notVeryImportantInformation.iDontKnow() * veryImportantInformation.thisIsUnnecessary();
+        return losingTrackOfTheCalculations;
+    }
+    public static String iAddedTheseLines(){
+        NotVeryImportantInformation notVeryImportantInformation = new NotVeryImportantInformation(1, 1);
+        VeryImportantInformation veryImportantInformation = new VeryImportantInformation(1, 1, 1);
+        return veryImportantInformation.functionWithString() + " " + notVeryImportantInformation.obligatoryUseOfString();
+    }
+    public static double mightAsWellUseAllFunctions(double data1, double data2, double data3, double data4, int modifier){
+        NotVeryImportantInformation notVeryImportantInformation = new NotVeryImportantInformation(data1, data2);
+        VeryImportantInformation veryImportantInformation = new VeryImportantInformation(data3, data4, modifier);
+        return notVeryImportantInformation.calculateUnclearResults() * veryImportantInformation.iAddedAnInteger();
     }
 }
--- 03_klassikomplekt/klassikomplekt/src/VeryImportantInformation.java
+++ 03_klassikomplekt/klassikomplekt/src/VeryImportantInformation.java
@@ -1,2 +1,26 @@
 public class VeryImportantInformation {
+    String importantInfo;
+    double usefulData;
+    double varThatLetsMeGetFreeFunctions;
+    int actualInteger;
+
+    public VeryImportantInformation(double data1, double data2, int data3){
+        this.importantInfo = "im not good at getting myself to actually do the things i need to do";
+        this.usefulData = data1;
+        this.varThatLetsMeGetFreeFunctions = data2;
+        this.actualInteger = data3;
+    }
+
+    public double thisIsUnnecessary(){
+        return varThatLetsMeGetFreeFunctions * (usefulData / 4);
+    }
+    public String functionWithString(){
+        return importantInfo;
+        //i forgot how to multiply a string and im currently lacking the brain function to remember
+        //this specific set of code all works on it technically meeting the requirements
+        //im not doing this out of spite, im just very much out of ideas
+    }
+    public double iAddedAnInteger(){
+        return varThatLetsMeGetFreeFunctions / actualInteger;
+    }
 }
